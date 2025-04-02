34e3a4d56354ac7e6ea52bfada52031fe77ba5af
vaibhav-chaudhari6696 <84838129+vaibhav-chaudhari6696@users.noreply.github.com>
2025-04-02 20:55:36 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 # Start the backend service
 cd /app/backend
-npm start &
+PORT=$BACKEND_PORT npm start &
 
 # Start the frontend service
 cd /app/frontend
-npm start &
+REACT_APP_API_URL=http://localhost:$BACKEND_PORT PORT=$FRONTEND_PORT npm start &
 
 # Wait for all background processes to complete
 wait 

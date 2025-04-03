c4fdf28014a6443a30e1b9f7106e1477a14f2578
vaibhav-chaudhari6696 <84838129+vaibhav-chaudhari6696@users.noreply.github.com>
2025-04-03 11:30:29 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,14 @@
 # Get the port from Render's environment variable
 RENDER_PORT=${PORT:-3000}
 
+# Start the frontend service
+cd /app/frontend
+REACT_APP_API_URL=http://localhost:9999 PORT=$RENDER_PORT npm start &
+
 # Start the backend service
 cd /app/backend
 PORT=$((RENDER_PORT - 1)) npm start &
 
-# Start the frontend service
-cd /app/frontend
-REACT_APP_API_URL=http://localhost:9999 PORT=$RENDER_PORT npm start &
 
 # Wait for all background processes to complete
 wait 
